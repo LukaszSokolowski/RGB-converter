da8ad592cf7c269a8c815d0b4f11f8de3c28820a
Łukasz Sokołowski <lukasz.m.sokolowski1995@gmail.com>
2018-11-15 19:42:35 +0100

    New view controller for displaying coverted values

---
+++ ColorZ/ResultsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ResultsViewController.swift
+//  ColorZ
+//
+//  Created by Łukasz Sokołowski on 15/11/2018.
+//  Copyright © 2018 Łukasz Sokołowski. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    @IBOutlet weak var rLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        rLabel.text = "Siemma"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
