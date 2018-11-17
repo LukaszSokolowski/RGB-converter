50c3baddc78502e0b9893c9995c75804aa5a840e
Łukasz Sokołowski <lukasz.m.sokolowski1995@gmail.com>
2018-11-17 02:56:23 +0100

    RGB to HSL  method added.

--- ColorZ/ResultsViewController.swift
+++ ColorZ/ResultsViewController.swift
@@ -31,6 +31,15 @@ class ResultsViewController: UIViewController {
     
     @IBOutlet weak var hexLabel: UILabel!
     
+    @IBOutlet weak var hLabelFromHsv: UILabel!
+    @IBOutlet weak var sLabelFromHsv: UILabel!
+    @IBOutlet weak var vLabelFromHsv: UILabel!
+    
+    @IBOutlet weak var hLabelFromHsl: UILabel!
+    @IBOutlet weak var sLabelFromHsl: UILabel!
+    @IBOutlet weak var lLabelFromHsl: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setStaticLabels()
@@ -38,9 +47,9 @@ class ResultsViewController: UIViewController {
         rgb2cmyk(red: redValue, green: greenValue, blue: blueValue)
         paintColorPreview()
         setHexLabel()
-        
-        var tablica = rgb2hsv(red: Float(redValue), grn: Float(greenValue), blu: Float(blueValue))
-        print(tablica)
+        rgb2hsl()
+        rgb2hsv()
+     
     }
 
     
@@ -99,15 +108,17 @@ class ResultsViewController: UIViewController {
     }
     
    
-    func rgb2hsv(red: Float, grn: Float, blu: Float) -> [Float] {
+    func rgb2hsv() {
+        var red = Float(redValue)
+        var grn = Float(greenValue)
+        var blu = Float(blueValue)
+        
         var hue: Float
         var sat: Float
         var val: Float
         var x: Float
         var f: Float
         var i: Float
-        var result: [Float] = [0,0,0]
-    
         x = min(red, grn, blu)
         val = max(red, grn, blu)
         
@@ -121,11 +132,49 @@ class ResultsViewController: UIViewController {
             hue = ((i-f/(val-x))*60).truncatingRemainder(dividingBy: Float(360))
             sat = ((val-x)/val)
         }
-        result[0] = hue
-        result[1] = sat
-        result[2] = val/255
-        return result
+        hLabelFromHsv.text = String(hue)
+        sLabelFromHsv.text = String(sat)
+        vLabelFromHsv.text = String(val/255)
+     
+    }
+    
+    func rgb2hsl() {
+        let redConverted = max(min(Float64(redValue)/255, 1), 0)
+        let greenConverted = max(min(Float64(greenValue)/255, 1), 0)
+        let blueConverted = max(min(Float64(blueValue)/255, 1), 0)
+        let maxValue = max(redConverted, greenConverted, blueConverted)
+        let minValue = min(redConverted, greenConverted, blueConverted)
+        let lightness = (maxValue + minValue) / 2
+        var hue = 0.0, saturation = 0.0, difference = 0.0
+        
+        if maxValue != minValue {
+            difference = maxValue - minValue
+            if lightness > 0.5 {
+                saturation = difference / (2 - maxValue - minValue)
+            } else {
+                saturation = difference / (maxValue + minValue)
+            }
+            if maxValue == redConverted {
+                if greenConverted < blueConverted {
+                    hue = (greenConverted - blueConverted) / difference + 6
+                } else {
+                    hue = (greenConverted - blueConverted) / difference
+                }
+            } else if maxValue == greenConverted {
+                hue = (blueConverted - redConverted) / difference + 2
+            } else {
+                hue = (redConverted - greenConverted) / difference + 4
+            }
+        } else {
+            hue = 0
+            saturation = 0
+        }
+        hLabelFromHsl.text = String(Int(hue * 60))
+        sLabelFromHsl.text = String(Int(saturation * 100))
+        lLabelFromHsl.text = String(Int(lightness * 100))
+    
         
     }
+  
 }
 
